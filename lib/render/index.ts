ea7e50c9c37e9f4d75bca9bc9f798eaf503b274b
webytool8-cell <webytool8@gmail.com>
2026-01-27 10:18:22 -0500

    Update index.ts

--- lib/render/index.ts
+++ lib/render/index.ts
@@ -1,7 +1,8 @@
 // lib/render/index.ts
-import { renderSVG } from './svg';
 
-// Optional wrapper for multiple formats (currently just SVG)
+import { renderSVG } from "./svg";
+
+// Optional wrapper for multiple formats
 export function renderFormats(vector: any) {
   return { svg: renderSVG(vector) };
 }
