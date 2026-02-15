9dd5f827799d311c546d7cce5f728af29e5588ee
webytool8-cell <webytool8@gmail.com>
2026-02-15 17:54:49 -0500

    Update Logo.js

--- public/site/components/Logo.js
+++ public/site/components/Logo.js
@@ -1,5 +1,5 @@
 function Logo({ className = "w-6 h-6", color = "currentColor" }) {
-    const logoUrl = "https://app.trickle.so/storage/public/images/usr_19c684eaa8000001/18d4bc74-04d9-4470-9169-71d5e0b57924.Untitled";
+    const logoUrl = "https://app.trickle.so/storage/public/images/usr_19c684eaa8000001/0b715318-c454-4fa4-862a-8ff08f6a6c21.png";
     
     return (
         <img 
