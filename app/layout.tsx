27bc2348d937a2de1e194f6ad2995039925e799a
webytool8-cell <webytool8@gmail.com>
2026-03-02 17:52:08 -0500

    Add Google Analytics gtag script to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,26 @@
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TRQDG2B4RV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-TRQDG2B4RV');`}
+        </Script>
+      </body>
     </html>
   );
 }
